80bfe3aa5cac425084b17e3ee91cc162c34b0130
eddyzera <edgar.nascimento@newgo.com.br>
2022-10-27 23:52:43 -0300

    [refactor]: created space and font size values

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -13,5 +13,38 @@ export const { globalCss, styled } = createStitches({
       ...blue,
       ...blueDark
     },
+    space: {
+      1: '0.25rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      5: '1.5rem',
+      6: '2rem',
+      7: '2.5rem',
+      8: '3rem',
+      9: '3.5rem',
+      10: '4rem',
+      11: '4.5rem',
+      12: '5rem'
+    },
+    fontSizes: {
+      1: '0.25rem',
+      2: '0.5rem',
+      3: '0.75rem',
+      4: '1rem',
+      5: '1.5rem',
+      6: '2rem',
+      7: '2.5rem',
+      8: '3rem',
+      9: '3.5rem',
+      10: '4rem',
+      11: '4.5rem',
+      12: '5rem'
+    },
+    radii: {
+      sm: '8px',
+      md: '16px',
+      lg: '100%'
+    }
   }
 })
