825782c9305dabca2fb9a4c249a97356b89b84e8
eddyzera <edgar.nascimento@newgo.com.br>
2022-11-10 18:23:34 -0300

    [test]: should input without errors and without icon

---
+++ src/components/Global/Input/__test__/Input.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react'
+import { PersonIcon } from '@radix-ui/react-icons'
+import { Input } from '..'
+
+describe('<Input />', () => {
+  it('should render input without errors', () => {
+    render(
+      <Input
+        inputId='name'
+        placeholder='ex: Jonh Doe'
+        icon={<PersonIcon />}
+        type="text"
+      />
+    )
+
+    const input = screen.getByPlaceholderText('ex: Jonh Doe')
+    expect(input).toBeTruthy()
+  })
+
+  it('should render input without icon', () => {
+    render(
+      <Input
+        inputId='name'
+        placeholder='ex: Jonh Doe'
+        type="text"
+      />
+    )
+
+    const input = screen.getByPlaceholderText('ex: Jonh Doe')
+    expect(input).toBeTruthy()
+  })
+})
